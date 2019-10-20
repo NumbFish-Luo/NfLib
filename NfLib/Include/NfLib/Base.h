544050627e119993e18e785ee28143a219f6ab2b
NumbFish <numbfish@yeah.net>
2019-10-21 00:17:04 +0800

    更新预编译选项

--- NfLib/Include/NfLib/Base.h
+++ NfLib/Include/NfLib/Base.h
@@ -7,9 +7,23 @@
 #define _LAYER(layer, name) 1
 
 #if _LAYER(0, "macro") /////////////////////////////////////////////////////////
+
+#define __WINDOWS_10__ 1
+#define __LPC177X_8X__ 0
+
+#if __WINDOWS_10__
+
 #include <Windows.h>
 #define GET_TICK_COUNT() (u32)GetTickCount64()
 #pragma warning(disable:4996)
+
+#elif __LPC177X_8X__
+
+#include "base/time.h"
+#define GET_TICK_COUNT() get_tick()
+
+#endif
+
 #endif // _LAYER(0, "typedef")
 
 #if _LAYER(0, "typedef") ///////////////////////////////////////////////////////
@@ -17,15 +31,23 @@ typedef bool        Bit;
 typedef char*       Str;
 typedef const char* CStr;
 
+#if __WINDOWS_10__
+
 typedef char      i8;
 typedef short     i16;
 typedef int       i32;
 typedef long long i64;
 
+#endif
+
 typedef unsigned char      u8, Byte;
 typedef unsigned short     u16, Word;
 typedef unsigned int       u32, DWord;
 typedef unsigned long long u64, QWord;
+
+typedef struct {
+    Byte byte[1];
+} u8x1;
 
 typedef union {
     Word word[1];
@@ -50,6 +72,7 @@ typedef union {
 Bit GetBit(Byte byte, Byte i);
 void SetBit(Byte* byte, Byte i, Bit data);
 void EndianReverse(Byte byte[], size_t size);
+u8x1 ToU8x1(u8  value);
 u8x2 ToU8x2(u16 value);
 u8x4 ToU8x4(u32 value);
 u8x8 ToU8x8(u64 value);
